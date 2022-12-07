7dec0f2a5a73737cce1eaaf2efe3ad5033092bdb
scottCourtney <m.scottcourntey@gmail.com>
2022-12-07 15:03:05 -0500

    Initial Commit

---
+++ home/homeApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeApp.swift
+//  home
+//
+//  Created by Scott Courtney on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct homeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
